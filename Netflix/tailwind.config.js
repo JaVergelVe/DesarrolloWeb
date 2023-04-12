5187e48226b31f60540128c7853135a6659ab873
JaVergelVe <javergelve@ufpso.edu.co>
2023-04-12 15:03:52 -0500

    Ejercicio de netflix casi terminado

--- Netflix/tailwind.config.js
+++ Netflix/tailwind.config.js
@@ -2,10 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '90': '90%',
+        '50': '3.12em'
+      },
+      backgroundImage: {
+        'fondo': "url('../src/img/destiny-2-beyond-light-uhdpaper.com-hd-5.2062.jpg')"
+      },
+      margin: {
+        '6.25': '6.25'
+      },
+      maxWidth: {
+        '1/2': '50%',
+      }
+    },
     fontFamily: {
       "body": ['Open Sans', 'sans-serif'],
       "name": ['Bebas Neue', 'cursive']
+    },
+    fontSize: {
+      '40': '40px',
+      '16': '16px',
+      '50': '3.12em',
+      '18': '1.12em',
+    },
+    minHeight: {
+      '650': '40.62em'
     }
   },
   plugins: [],
