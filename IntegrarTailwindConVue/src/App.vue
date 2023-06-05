ebf0a5128df2c02d98d96ff194bc356bdb6c3d8c
JaVergelVe <javergelve@ufpso.edu.co>
2023-06-05 12:04:49 -0500

    Se agrego la funcion de agregar tarjeta

--- IntegrarTailwindConVue/src/App.vue
+++ IntegrarTailwindConVue/src/App.vue
@@ -18,10 +18,27 @@
     users.value.splice(index, 1)
     console.log(users)
   }
+
+  let name, mail
+
+  const addUser = () => {
+    const newUser = {
+      name: name,
+      mail: mail
+    }
+
+    users.value.push(newUser)
+  }
 </script>
 
 <template>
   <h1 class=" text-2xl">Hola mundo</h1>
+
+  <input type="text" v-model="name" placeholder="Escriba su nombre" class=" border rounded">
+  <br> <br>
+  <input type="text" v-model="mail" placeholder="Escriba su correo" class=" border rounded">
+  <br>
+  <button class=" bg-green-200 px-2 py-1 rounded mt-2" @click="addUser">Añadir</button>
 
   <div class=" space-y-2">
     <CardComponent v-for="(user, index) in users" :key="index" :name="user.name" :mail="user.mail" :index="index" @borrarTarjeta="borrarTarjeta" />
